b4cf9c74412ef9f7acf514317c4b90e9049059ba
JaanusFuzzing <jaanus.kaap+github@gmail.com>
2015-11-24 10:37:20 +0200

    ADB detector also extracts tombstone file and reads the more exact location of the crash

--- Vanapagan/Detector/AndroidAdb.py
+++ Vanapagan/Detector/AndroidAdb.py
@@ -2,6 +2,8 @@ from ..CrashReport import CrashReport
 import time
 import select
 import subprocess
+import tempfile
+import os
 
 class AndroidAdb:
 	adbExecutable = "adb"
@@ -24,10 +26,12 @@ class AndroidAdb:
 		if self.device == None:
 			subprocess.call([self.adbExecutable, "push", testFile, self.deviceTmpFile])
 			subprocess.call([self.adbExecutable, "shell", "logcat", "-c"])
+			subprocess.call([self.adbExecutable, "shell", "su", "-c","rm /data/tombstones/*"])
 			subprocess.call([self.adbExecutable, "shell", "am", "start", "-a" "android.intent.action.VIEW", "-n", package, "file://" + self.deviceTmpFile])
 		else:
 			subprocess.call([self.adbExecutable, "-s", self.device, "push", testFile, self.deviceTmpFile])
 			subprocess.call([self.adbExecutable, "-s", self.device, "shell", "logcat", "-c"])
+			subprocess.call([self.adbExecutable, "-s", self.device, "shell", "su", "-c","rm /data/tombstones/*"])
 			subprocess.call([self.adbExecutable, "-s", self.device, "shell", "am", "start", "-a" "android.intent.action.VIEW", "-n", package, "file://" + self.deviceTmpFile])
 				
 	def waitForCrash(self, waitTime = 4):
@@ -52,6 +56,7 @@ class AndroidAdb:
 			lines = result.split("\n")
 			for x in xrange(len(lines)):
 				if "(SIG" in lines[x]:
+					#logcat data
 					cols = lines[x].split(" ")
 					if len(cols)>0:
 						report.location = cols[0].replace("/", ".").replace(":", ".")
@@ -66,6 +71,31 @@ class AndroidAdb:
 						pos2 = pos1 + lines[x][pos1:].find(")")
 						if pos2-pos1>0:
 							report.type = lines[x][pos1:pos2]
+							
+					#tombstone
+					content = ""
+					(fhandler, fname) = tempfile.mkstemp()
+					os.close(fhandler)
+					if self.device == None:
+						subprocess.call([self.adbExecutable, "shell", "su", "-c", "chmod 777 /data/tombstones"])
+						subprocess.call([self.adbExecutable, "shell", "su", "-c", "chmod 777 /data/tombstones/tombstone_00"])
+						subprocess.call([self.adbExecutable, "pull", "/data/tombstones/tombstone_00", fname])
+					else:
+						subprocess.call([self.adbExecutable, "-s", self.device, "shell", "su", "-c", "chmod 777 /data/tombstones"])
+						subprocess.call([self.adbExecutable, "-s", self.device, "shell", "su", "-c", "chmod 777 /data/tombstones/tombstone_00"])
+						subprocess.call([self.adbExecutable, "-s", self.device, "pull", "/data/tombstones/tombstone_00", fname])
+					with open(fname, 'r') as content_file:
+						content = content_file.read()
+					report.info += "\n\n" + content
+
+					#Let's now parse the shit out of the tombstone data
+					pos1 = content.find("backtrace")
+					pos1 += content[pos1:].find("\n") + 1
+					pos2 = pos1 + content[pos1:].find("\n")
+					parts = content[pos1:pos2].split()
+					if parts[0] == "#00":
+						parts2 = parts[3].split("/")
+						report.location = parts2[len(parts2)-1] + "_" + parts[2]
 			return report		
 		return None
 				
